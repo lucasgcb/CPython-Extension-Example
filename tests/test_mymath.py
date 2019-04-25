a2e19a86ef44bfa60f9d7d6b7b4ff0cdc6c53519
lucasgcb <lucascoliado@hotmail.com>
2019-04-25 12:55:09 +0200

    Fix test in the wrong place, update gitignore

--- tests/test_mymath.py
+++ tests/test_mymath.py
@@ -9,7 +9,3 @@ def test_add():
     errMsg = "Add not adding correctly"
     result = mymath.add(2,2)
     assert result == 4, errMsg
-
-def test_xablau():
-    errMsg = "System returned non-zero for xablau"
-    assert mymath.xablau() == 0, errMsg
