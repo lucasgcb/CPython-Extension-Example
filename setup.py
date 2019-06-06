d0c7d8950c80a0a15826133fe8a938785251ffff
lucasgcb <lucascoliado@hotmail.com>
2019-06-06 11:38:58 +0200

    up version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ module2 = Extension('pyextensions.mymath',
                     sources = ['src/pyextensions/mymath.c'])
 
 
-VERSION = "0.2.0"
+VERSION = "0.2.3"
 
 def readme():
     """print long description"""
